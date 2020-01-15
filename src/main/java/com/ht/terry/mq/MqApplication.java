512f41479a5d50d51a03d5d7e65b465c9e4a52fc
2523796216@qq.com <123456wh@>
2020-01-15 10:34:05 +0800

    upload test

---
+++ src/main/java/com/ht/terry/mq/MqApplication.java
@@ -0,0 +1,18 @@
+package com.ht.terry.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：terry
+ * @date ：Created in 2020/1/9 10:15
+ * @description：TODO
+ * @modified By：
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class MqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MqApplication.class,args);
+    }
+}
